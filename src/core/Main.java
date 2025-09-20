ae28ae911354a0aafaad4df2d9d75999b99fde70
goodkosawanichakit <goodkosawanichakit@gmail.com>
2025-09-20 12:48:14 +0700

    dev 0.1

--- src/core/Main.java
+++ src/core/Main.java
@@ -1,18 +1,43 @@
 import java.util.Scanner;
 
+enum Color {NONE, RED, GREEN, BLUE, YELLOW};
+
+class Position {
+  public int row;
+  public int col;
+  Position(int r, int c) {this.row = r; this.col = c;}
+}
+
+class Block {
+  public Position pos;
+  public Color color;
+  Block(int r, int c, Color color) {
+    pos = new Position(r, c);
+    this.color = color;
+  }
+  int getR() {return pos.row;}
+  int getC() {return pos.col;}
+}
+
 class Board {
   private int row = 20;
   private int col = 10;
-  public int board[] = new int[row * col];
+  public Block board[] = new Block[row * col];
 
   synchronized void printBoard() {
     System.out.print("\033[H\033[2J"); 
     System.out.flush();
     for (int i = 0; i < board.length; i++) {
-      if (board[i] == 0)
+      if (board[i].color == Color.NONE)
         System.out.print('.');
-      else if (board[i] == 1)
-        System.out.print('O');
+      else if (board[i].color == Color.RED)
+        System.out.print('R');
+      else if (board[i].color == Color.GREEN)
+        System.out.println('G');
+      else if (board[i].color == Color.BLUE)
+        System.out.println('B');
+      else if (board[i].color == Color.YELLOW)
+        System.out.println('Y');
       if (i % col == col - 1)
         System.out.println(); 
     }  
@@ -20,9 +45,9 @@ class Board {
 
   synchronized void updatePhysics() {
     for (int i = ((row - 1) * col) - 1; i >= 0; i--) {
-      if (board[i] != 0 && board[i + col] == 0) {
+      if (board[i] != null && board[i + col] == null) {
         board[i + col] = board[i];
-        board[i] = 0;
+        board[i] = null;
       }
     }
   }
@@ -31,7 +56,8 @@ class Board {
 public class Main {
   public static void main(String args[]) throws InterruptedException {
     Board board = new Board();
-    board.board[6] = 1;
+    Block red = new Block(6, 1, Color.RED);
+    board.board[red.getC() * red.getR()] = red;
 
     Thread physicsThread = new Thread(() -> {
       try {
